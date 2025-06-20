74a2ea9d0c76539b11a9e7136f510d1449f17480
Baljeetsaini22 <baljeetsaini7440@gmail.com>
2025-06-20 09:24:51 +0530

    Fix href attribute for "See Menu" link to ensure proper navigation

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,7 +6,7 @@ const Hero = () => {
           <h1 className="text-center text-5xl">Welcome To HariOm भोजनालय</h1>
           <p className="py-5">Delicious Taste of Tradition</p>
           <div className="flex justify-between gap-10 text-center">
-            <a id="#menu" className="cursor-pointer w-30 py-2 rounded-2xl bg-gradient-to-r from-yellow-400 to-white text-black">
+            <a href="#menu" className="cursor-pointer w-30 py-2 rounded-2xl bg-gradient-to-r from-yellow-400 to-white text-black">
               See Menu
             </a>
             <a className="cursor-pointer w-30 py-2 rounded-2xl bg-gradient-to-l from-yellow-400 to-white text-black">
