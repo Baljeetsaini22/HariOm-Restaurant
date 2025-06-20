ca73fc0772ee1eac61b240a8d329dbd38b903d28
Baljeetsaini22 <baljeetsaini7440@gmail.com>
2025-06-20 10:11:35 +0530

    Implement code changes to enhance functionality and improve performance

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,9 @@
 const Hero = () => {
   return (
-    <section className="hero-section w-full text-white bg-[url(./assets/images/hero-banner.png)] bg-cover">
-      <div className="bg-[#00000080] h-[60vh] flex items-center justify-center">
+    <section className="hero-section w-full text-white bg-[url('/banner/banner.jpg')] bg-cover">
+      <div className="bg-[#00000080] h-[50vh] md:h-[100vh] flex items-center justify-center">
         <div className=" flex flex-col items-center">
-          <h1 className="text-center text-5xl">Welcome To HariOm भोजनालय</h1>
+          <h1 className="text-center text-3xl md:text-5xl">Welcome To HariOm भोजनालय</h1>
           <p className="py-5">Delicious Taste of Tradition</p>
           <div className="flex justify-between gap-10 text-center">
             <a href="#menu" className="cursor-pointer w-30 py-2 rounded-2xl bg-gradient-to-r from-yellow-400 to-white text-black">
